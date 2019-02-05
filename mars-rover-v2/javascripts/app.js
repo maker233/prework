2d9d5199b2376afb1bf5b16ab9e3b231057fc481
Javier <javiermaker@gmail.com>
2019-02-05 20:45:41 +0100

    Primera versión

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,8 +1,26 @@
 // Rover Object Goes Here
 // ======================
-
+var rover = {
+  direction: "N";
+  positions: ["N","S","E","W"];
+  coord: [a,b];
+}
 // ======================
 function turnLeft(rover){
+  switch(rover.direction) {
+    case N:
+      rover.direction = "W";
+      breack;
+    case S:
+      rover.direction = "E";
+      breack;
+    case W:
+      rover.direction = "S";
+      breack;
+    case E:
+      rover.direction = "N";
+      breack;
+  }
   console.log("turnLeft was called!");
 }
 
@@ -13,3 +31,9 @@ function turnRight(rover){
 function moveForward(rover){
   console.log("moveForward was called")
 }
+
+function moveBack(rover) {
+  console.log("moveForward was called")
+}
+
+var = testzone 
